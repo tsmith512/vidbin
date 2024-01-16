cc1742e4dce5fdadcdcf25ad8bcd495502ba7f7f
Taylor Smith <tsmith@tsmithcreative.com>
2024-01-16 12:18:53 -0600

    Add a route for video info polling

---
+++ src/app/api/stream/get-video-info/route.ts
@@ -0,0 +1,60 @@
+import type { NextRequest } from 'next/server';
+
+export const runtime = 'edge';
+export const fetchCache = 'force-no-store';
+
+// These are stored in environment variables on Pages (or Workers or your own
+// origin application) because we don't want the public to get a Stream API key.
+const key = process.env.ACCOUNT_KEY;
+const tag = process.env.ACCOUNT_TAG;
+
+/**
+ * Ask Stream about information for a video. We'll need this to know if a video
+ * is ready to play.
+ *
+ * @param request
+ * @returns
+ */
+export async function POST(request: NextRequest) {
+  const input = await request.json();
+  const video_id = input.video_id || false;
+
+  if (!video_id) {
+    return new Response('No Video ID provided', { status: 400 });
+  }
+
+  const options = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${key}`,
+    },
+  };
+
+  const response = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${tag}/stream/${video_id}`,
+    options
+  );
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    return new Response('error', { status: 500 });
+  }
+
+  return new Response(
+    JSON.stringify({
+      // Stream Video ID
+      video_id: data.result.uid,
+      status: data.result.status.state,
+      name: data.result.meta.name,
+      duration: data.result.duration,
+      created: data.result.created,
+    }),
+    {
+      headers: {
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Origin': '*',
+      },
+    }
+  );
+}
