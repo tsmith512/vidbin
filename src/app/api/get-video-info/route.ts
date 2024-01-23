41f676597a8a543d222616265e15efdaf49ea57b
Taylor Smith <tsmith@tsmithcreative.com>
2024-01-22 23:09:00 -0600

    Add an endpoint within the app for status

---
+++ src/app/api/get-video-info/route.ts
@@ -0,0 +1,52 @@
+import type { NextRequest } from 'next/server';
+
+export const runtime = 'edge';
+export const fetchCache = 'force-no-store';
+
+// These are stored in environment variables on Pages (or Workers or your own
+// origin application) because we don't want the public to get a Stream API key.
+const key = process.env.ACCOUNT_KEY;
+const tag = process.env.ACCOUNT_TAG;
+
+/**
+ * Ask our own database for video info. Stream is telling us (via the webhook)
+ * when a video is ready to play, so we just need to pass that info back to the
+ * user. We can also provide other attributes we've stored, like name and date.
+ *
+ * @param request
+ * @returns
+ */
+export async function POST(request: NextRequest) {
+  const input = await request.json();
+  const id: number | false = input?.id || false;
+
+  if (!id) {
+    return new Response('No VidBin ID provided', { status: 400 });
+  }
+
+  if (typeof id !== 'number') {
+    return new Response('VidBin ID is numeric. (Did you send a Stream ID?)', { status: 400 });
+  }
+
+  const result = await process.env.DB.prepare(`SELECT * FROM videos WHERE id = ?`).bind(id).first();
+
+  if (!result) {
+    return new Response('VidBin not found.', { status: 404 });
+  }
+
+  return new Response(
+    JSON.stringify({
+      id: result.id,
+      video_id: result.video_id,
+      status: result.status,
+      name: result.name,
+      created: result.created,
+      // @TODO: We return duration from /api/stream/get-video-info, add it to the DB or scratch from both?
+    }),
+    {
+      headers: {
+        'Content-type': 'application/json',
+      },
+    }
+  );
+}
--- src/app/api/stream/README.md
+++ src/app/api/stream/README.md
@@ -1,3 +1,8 @@
 This directory contains server-side routes, each of which makes calls back to
 Cloudflare Stream. I tried to name these routes as obviously as I could and
 document each of them in code.
+
+- **Get Direct Upload URL:** Provision an endpoint that a VidBin user can post
+  a video to directly.
+- **Get Video Info:** A proxy of video status information. Useful earlier in
+  development, but once we start storing that info in our DB instead, not needed.
