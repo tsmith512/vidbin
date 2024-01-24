cc7684c9f98bb32ac58367d348d13d8fe5a06f93
Taylor Smith <tsmith@tsmithcreative.com>
2024-01-24 16:48:58 -0600

    What if... instead of a zillion console.logs in a desperate attempt to debug this silliness... we built a fancy educational component and called it a feature of my presentation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,11 +19,19 @@ enum sources {
 export default function Home() {
   const router = useRouter();
   const [uploading, setUploading] = useState(false);
+  const [uploadingMessages, setUploadingMessages] = useState<string[]>([]);
   const [inputSource, setInputSource] = useState<sources>(sources.file);
 
   const uploadHandler = async (input: uploadHandlerProps): Promise<void> => {
     // @TODO: Check for file-size. Current request is a 200MB max basic upload URL
     setUploading(true);
+
+    // Educational: explain what's happening below the form:
+    setUploadingMessages(['Upload handler called']);
+    setUploadingMessages((previously) => [
+      ...previously,
+      'Asking VidBin server for a direct upload URL.',
+    ]);
 
     // Query a server-side function to provision us a direct upload URL
     const response = await fetch('/api/stream/get-direct-upload-url', {
@@ -32,16 +40,34 @@ export default function Home() {
       body: JSON.stringify({ name: input.name }),
     });
 
+    // If VidBin errored on us, we don't have a Direct Upload endpoint to send
+    // this file to and/or we probably also don't have a row in VidBin's DB.
     if (!response.ok) {
-      alert('Failed to get a direct upload URL. Aborting.');
+      const fullTextResponse = await response.text();
+      setUploadingMessages((previously) => [
+        ...previously,
+        `VidBin API returned ${response.status} ${response.statusText}: ${fullTextResponse}`,
+      ]);
+      setUploading(false);
       return;
     }
 
-    // Get our endpoint from the VidBin API response:
+    // Get our endpoint from the VidBin API response and explain.
     const data = await response.json();
-    console.log(`Received direct upload URL: ${data.endpoint}`)
+    setUploadingMessages((previously) => [
+      ...previously,
+      `VidBin ID will be ${data.id}. Stream ID will be ${data.video_id}.`,
+    ]);
+    setUploadingMessages((previously) => [
+      ...previously,
+      `Stream Direct Upload URL: ${data.endpoint}`,
+    ]);
 
     // Build a FormData to submit on:
+    setUploadingMessages((previously) => [
+      ...previously,
+      `Setting up direct upload payload.`,
+    ]);
     const submission = new FormData();
     submission.append('name', input.name);
 
@@ -53,21 +79,35 @@ export default function Home() {
       new File([input.file], input.name, { type: input.file.type })
     );
 
-    console.log(submission);
+    setUploadingMessages((previously) => [...previously, `Starting transfer.`]);
 
+    // This is a post of our file/filename to Stream directly. This way VidBin
+    // doesn't have to handle proxying or storing the video file itself. Keeps
+    // our side "lean," and we did it without exposing Stream API secrets to
+    // end-user devices.
     const result = await fetch(data.endpoint, {
       method: 'POST',
       body: submission,
     });
 
+    setUploadingMessages((previously) => [
+      ...previously,
+      `Transfer response: ${result.status} ${result.statusText}`,
+    ]);
     console.log(result);
 
     // Watch out! We're looking for an `ok` from the Stream Direct Upload endpoint
     // but using the VidBin API's response (`data`) for the ID of the new page.
     if (result.ok) {
+      setUploadingMessages((previously) => [...previously, `Loading viewer`]);
       router.push(`/view/${data.id}`);
     } else {
-      alert(`Direct upload failed: ${result.status} ${result.statusText}`);
+      const fullResponse = await result.json();
+      setUploadingMessages((previously) => [
+        ...previously,
+        `Upload failed. ${result.status} ${result.statusText}: ${fullResponse}`,
+      ]);
+      setUploading(false);
     }
   };
 
@@ -121,6 +161,22 @@ export default function Home() {
     <>
       <h2>Upload a video</h2>
       {uploading ? uploadingStage() : inputStage()}
+
+      {uploadingMessages.length > 0 && (
+        <div className="upload-progress">
+          <div
+            className="divider text-center upload-progress-divider"
+            data-content="UNDER THE HOOD"
+            style={{ margin: '2rem 0' }}
+          ></div>
+
+          <ul className="upload-progress-messages">
+            {uploadingMessages.map((message, i) => (
+              <li key={i}>{message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </>
   );
 }
